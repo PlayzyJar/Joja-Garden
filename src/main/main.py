ed94013a6347b557c0b0b24b8b83fe81d59b9c3f
jeffBrasil <jefferson.ribeiro.brasil@ccc.ufcg.edu.br>
2025-12-07 18:42:40 -0300

    Criação do token de acesso JWT e adição da rota de autenticação

--- src/main/main.py
+++ src/main/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 
-from .api.endpoints import admin_router
+from .api.endpoints import admin_router, auth_router
 
 app.include_router(admin_router.router, prefix="/admin", tags=["Admins"])
+
+app.include_router(auth_router.router, prefix="/auth", tags=["Autenticação"])
 # Para executar o código no terminal, utilizar unicorn main:app --reload
