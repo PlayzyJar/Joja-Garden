a94c19809a72b717d3ba4c67e4c80de492862d74
jeffBrasil <jefferson.ribeiro.brasil@ccc.ufcg.edu.br>
2025-12-07 20:14:25 -0300

    Rota para Admin criar usuario. US-2 e US-3

--- src/main/main.py
+++ src/main/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-from .api.endpoints import admin_router, auth_router
+from .api.endpoints import admin_router, auth_router, usuario_router
 
 app.include_router(admin_router.router, prefix="/admin", tags=["Admins"])
 
 app.include_router(auth_router.router, prefix="/auth", tags=["Autenticação"])
+
+app.include_router(usuario_router.router, prefix="/usuario", tags=["Usuario"])
 # Para executar o código no terminal, utilizar unicorn main:app --reload
