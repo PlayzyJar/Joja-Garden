43ff442eb47706218a49895fea11b8a95bb48931
Arthur Taveira <arthurguedes001@gmail.com>
2025-12-15 18:31:13 -0300

    Definicao de funcao para validar senha

---
+++ src/services/verificacoes.py
@@ -0,0 +1,35 @@
+def validar_cpf(cpf):
+  retorno = False
+  if len(cpf) != 11 or not cpf.isdigit():
+        return retorno
+
+  #Evita casos de números iguais
+  if cpf == cpf[0] * 11:
+        return retorno
+
+  #Cálculo do primeiro dígito verificador
+  soma = 0
+  for i in range(9):
+    soma += int(cpf[i]) * (10 - i)
+    
+    resto = (soma * 10) % 11
+    if resto == 10:
+        resto = 0
+        
+    if resto != int(cpf[9]):
+        return retorno
+    
+    #Cálculo do segundo dígito verificador
+    soma = 0
+    for i in range(10):
+        soma += int(cpf[i]) * (11 - i)
+        
+    resto = (soma * 10) % 11
+    if resto == 10:
+        resto = 0
+
+    if resto != int(cpf[10]):
+        return retorno
+
+    return True
+  
