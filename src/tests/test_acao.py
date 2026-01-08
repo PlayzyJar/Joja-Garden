fbe26bfe991440b55432f5f43e75b56d56833485
Arthur Taveira <arthurguedes001@gmail.com>
2026-01-08 10:26:31 -0300

    correção de alguns arquivos com erros de sintaxe não tolerados por maquina virtual // versao 2

--- src/tests/test_acao.py
+++ src/tests/test_acao.py
@@ -5,7 +5,7 @@ from fastapi import status
 
 @pytest.mark.parametrize("acao", ["poda", "rega", "adubo"])
 def test_registrar_acao_valida(client: TestClient, get_usuario_header_com_id, planta_usuario, acao):
-    response = client.post(f"/acao/{planta_usuario["id"]}/registrar", headers={"Authorization": get_usuario_header_com_id["Authorization"]}, json={"tipo": acao, "descricao": "string", "data_hora": "2025-12-24"})
+    response = client.post(f'acao/{planta_usuario["id"]}/registrar', headers={"Authorization": get_usuario_header_com_id["Authorization"]}, json={"tipo": acao, "descricao": "string", "data_hora": "2025-12-24"})
     assert response.status_code == status.HTTP_201_CREATED
     assert response.json()['tipo'] == acao
 
